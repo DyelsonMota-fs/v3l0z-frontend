285a5b875ef550941c426024e9ee10d9de7f56d9
DyelsonMota-fs <Dyelsonm.fs@hotmail.com>
2026-05-11 21:31:51 -0300

    perf: improve country flag image sizing

--- src/components/CountryCard.tsx
+++ src/components/CountryCard.tsx
@@ -19,6 +19,7 @@ export function CountryCard({ country }: CountryCardProps) {
           src={country.flags.svg}
           alt={country.flags.alt || `Bandeira de ${country.name.common}`}
           fill
+          sizes="(max-width: 640px) 100vw, (max-width: 1024px) 50vw, 25vw"
           className="object-cover"
         />
       </div>
