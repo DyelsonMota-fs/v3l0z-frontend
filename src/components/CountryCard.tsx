ca8a687315fcd31f33837c18b02a3451fe68b73a
DyelsonMota-fs <Dyelsonm.fs@hotmail.com>
2026-05-11 21:20:20 -0300

    fix: set relative position for country flag image

--- src/components/CountryCard.tsx
+++ src/components/CountryCard.tsx
@@ -1,8 +1,8 @@
 import Image from "next/image";
 import Link from "next/link";
 
-import { Country } from "@/app/types/country";
-import { formatNumber } from "@/app/utils/formatNumber";
+import type { Country } from "@/types/country";
+import { formatNumber } from "@/utils/formatNumber";
 
 type CountryCardProps = {
   country: Country;
